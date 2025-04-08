1f30fc21dfa0704f74ddc4498999774aacd2e79f
Eyupispir <Eypispir@outlook.com>
2025-04-08 06:08:06 +0300

    Add project files.

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox_Bird = new System.Windows.Forms.PictureBox();
+            this.pictureBox_pipeUp = new System.Windows.Forms.PictureBox();
+            this.pictureBox_pipeDown = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.pictureBox_Ground = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_pipeUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_pipeDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Bird
+            // 
+            this.pictureBox_Bird.Image = global::FlappyBird.Properties.Resources.imgbin_flappy_bird_blue_minecraft_png1;
+            this.pictureBox_Bird.Location = new System.Drawing.Point(115, 317);
+            this.pictureBox_Bird.Name = "pictureBox_Bird";
+            this.pictureBox_Bird.Size = new System.Drawing.Size(78, 58);
+            this.pictureBox_Bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Bird.TabIndex = 2;
+            this.pictureBox_Bird.TabStop = false;
+            // 
+            // pictureBox_pipeUp
+            // 
+            this.pictureBox_pipeUp.Image = global::FlappyBird.Properties.Resources.imgbin_mario_pipe_upsidedown_png;
+            this.pictureBox_pipeUp.Location = new System.Drawing.Point(1657, -389);
+            this.pictureBox_pipeUp.Name = "pictureBox_pipeUp";
+            this.pictureBox_pipeUp.Size = new System.Drawing.Size(124, 600);
+            this.pictureBox_pipeUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_pipeUp.TabIndex = 1;
+            this.pictureBox_pipeUp.TabStop = false;
+            this.pictureBox_pipeUp.Click += new System.EventHandler(this.pictureBox_pipeUp_Click);
+            // 
+            // pictureBox_pipeDown
+            // 
+            this.pictureBox_pipeDown.Image = global::FlappyBird.Properties.Resources.imgbin_mario_pipe_png;
+            this.pictureBox_pipeDown.Location = new System.Drawing.Point(1657, 421);
+            this.pictureBox_pipeDown.Name = "pictureBox_pipeDown";
+            this.pictureBox_pipeDown.Size = new System.Drawing.Size(124, 766);
+            this.pictureBox_pipeDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_pipeDown.TabIndex = 0;
+            this.pictureBox_pipeDown.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.AllowFocus = false;
+            this.simpleButton1.Location = new System.Drawing.Point(12, 12);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.ShowFocusRectangle = DevExpress.Utils.DefaultBoolean.False;
+            this.simpleButton1.Size = new System.Drawing.Size(103, 40);
+            this.simpleButton1.TabIndex = 3;
+            this.simpleButton1.Text = "Start";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            this.simpleButton1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.simpleButton1_MouseDoubleClick);
+            // 
+            // pictureBox_Ground
+            // 
+            this.pictureBox_Ground.Image = global::FlappyBird.Properties.Resources.kindpng_1153893__1_;
+            this.pictureBox_Ground.Location = new System.Drawing.Point(0, 606);
+            this.pictureBox_Ground.Name = "pictureBox_Ground";
+            this.pictureBox_Ground.Size = new System.Drawing.Size(612, 54);
+            this.pictureBox_Ground.TabIndex = 4;
+            this.pictureBox_Ground.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(140, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 103);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Game Over";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(598, 642);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox_Ground);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.pictureBox_Bird);
+            this.Controls.Add(this.pictureBox_pipeUp);
+            this.Controls.Add(this.pictureBox_pipeDown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_pipeUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_pipeDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Ground)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_pipeDown;
+        private System.Windows.Forms.PictureBox pictureBox_pipeUp;
+        private System.Windows.Forms.PictureBox pictureBox_Bird;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.PictureBox pictureBox_Ground;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,223 @@
+﻿using DevExpress.UIAutomation;
+using DevExpress.Utils.VisualEffects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+
+        public int pipesLocation;
+        Random rand = new Random();
+        PictureBox pipedown = new PictureBox();
+        PictureBox pipeup = new PictureBox();
+        bool birdown = true;
+        int bird;
+        bool spawned = true;
+        
+
+
+
+        public Form1( )
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+            pipeMove(4);
+            
+            if (birdown)
+            {
+                BirdDown();
+
+            }
+            else
+            {
+                bird = pictureBox_Bird.Location.Y;
+                while (bird - 60 <= pictureBox_Bird.Location.Y)
+                {
+
+                    BirdUp();
+
+                    birdown = true;
+                    
+
+                }
+            }
+
+            if (checkPipeOut()) { pipesRespawn(); }
+
+            
+
+        }
+        private void isEnd( )
+        {
+            if (birdCollide()) { timer1.Stop(); 
+            
+            button1.Enabled = true;
+            button1.Visible = true;
+            
+            }
+        }
+
+        private bool checkPipeOut( )
+        {
+            if (pipeup.Location.X <= -100) { return true; }
+            else { return false; }
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            gameStart();
+            simpleButton1.Visible = false;
+            simpleButton1.Enabled = false;
+            
+
+        }
+        private void gameStart( )
+        {
+            pictureBox_Bird.Location = new Point(100,300);
+            pipesSpawn();
+            timer1.Start();
+        }
+
+
+        private void orignalPipeMove( int a )
+        {
+            pictureBox_pipeDown.Location = new Point(pictureBox_pipeUp.Location.X -a,pictureBox_pipeDown.Location.Y);
+            pictureBox_pipeUp.Location = new Point(pictureBox_pipeUp.Location.X - a,pictureBox_pipeUp.Location.Y);
+
+        }
+
+        private void pictureBox_pipeUp_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pipesSpawn( )
+        {
+            Point location = new Point(700, rand.Next(100, 410));
+            pipesLocation = location.X;
+
+
+
+
+            pipeup.Location = new Point(location.X - (pictureBox_pipeUp.Size.Width / 2), location.Y + pictureBox_Bird.Size.Height * 2);
+            pipeup.Size = new Size(pictureBox_pipeUp.Size.Width, pictureBox_pipeUp.Size.Height);
+            pipeup.Image = Image.FromFile("C:\\Users\\eyup\\source\\repos\\FlappyBird\\FlappyBird\\Resources\\imgbin_mario-pipe-png.png");
+            pipeup.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(pipeup);
+
+
+
+
+            pipedown.Size = new Size(pictureBox_pipeDown.Size.Width, pictureBox_pipeDown.Size.Height);
+            pipedown.Location = new Point(location.X - (pictureBox_pipeDown.Size.Width / 2), location.Y - (pictureBox_pipeDown.Size.Height + pictureBox_Bird.Size.Height * 2));
+            pipedown.Image = Image.FromFile("C:\\Users\\eyup\\source\\repos\\FlappyBird\\FlappyBird\\Resources\\imgbin_mario-pipe-upsidedown-png.png");
+            pipedown.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(pipedown);
+            spawned = true;
+
+        }
+        private void pipesRespawn( )
+        {
+            Point location = new Point(700, rand.Next(100, 410));
+
+            pipeup.Location = new Point(location.X - (pictureBox_pipeUp.Size.Width / 2), location.Y + pictureBox_Bird.Size.Height * 2);
+            pipedown.Location = new Point(location.X - (pictureBox_pipeDown.Size.Width / 2), location.Y - (pictureBox_pipeDown.Size.Height + pictureBox_Bird.Size.Height * 2));
+
+        }
+        private void pipesDespawn( )
+        {
+            this.Controls.Remove( pipeup );
+            this.Controls.Remove(pipedown);
+        }
+
+        private void pipeMove(int a )
+        {
+            pipeup.Location = new Point(pipeup.Location.X - a , pipeup.Location.Y);
+            pipedown.Location = new Point(pipeup.Location.X - a, pipedown.Location.Y);
+
+
+        }
+        private void BirdDown( )
+        {
+            pictureBox_Bird.Location = new Point(pictureBox_Bird.Location.X, pictureBox_Bird.Location.Y + 3);
+            isEnd();
+        }
+        private void BirdUp( )
+        {
+
+            pictureBox_Bird.Location = new Point(pictureBox_Bird.Location.X, pictureBox_Bird.Location.Y - 3);
+            isEnd();
+
+
+        }
+
+        private void simpleButton1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            pipesDespawn();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+
+            birdown = false;
+            
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool birdCollide( )
+        {   
+            bool Flag = false;
+
+            if(pictureBox_Bird.Location.Y + pictureBox_Bird.Size.Height >= pictureBox_Ground.Location.Y) { Flag = true; }
+            if(pictureBox_Bird.Location.X+pictureBox_Bird.Size.Width >= pipedown.Location.X && pictureBox_Bird.Location.Y  <=  pipedown.Location.Y + pipedown.Size.Height && pipedown.Location.X + pipedown.Size.Width >= pictureBox_Bird.Location.X) Flag = true;
+            if(pictureBox_Bird.Location.X+pictureBox_Bird.Size.Width >= pipeup.Location.X && pictureBox_Bird.Location.Y + pictureBox_Bird.Size.Height >= pipeup.Location.Y && pipeup.Location.X+pipeup.Size.Width >= pictureBox_Bird.Location.X) Flag = true;
+
+
+            return Flag;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            gameStart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameStart();
+            button1.Enabled = false;
+            button1.Visible = false;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            birdown = false;
+        }
+    }
+}
